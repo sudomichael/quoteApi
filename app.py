e732719a4c835c859b99f5f382b83dd60582b781
sudomichael <devpmk@gmail.com>
2016-11-15 15:37:18 -0500

    can now get individual quote, can also add quote

--- app.py
+++ app.py
@@ -24,6 +24,35 @@ def index():
 def get_quotes():
     return jsonify({'quotes': quotes})
 
+from flask import abort
+
+@app.route('/api/v1.0/quotes/<int:quote_id>', methods=['GET'])
+def get_quote(quote_id):
+    quote = [quote for quote in quotes if quote['id'] == quote_id]
+    if len(quote) == 0:
+        abort(404)
+    return jsonify({'quote':quote[0]})
+
+from flask import make_response
+
+@app.errorhandler(404)
+def not_found(error):
+    return make_response(jsonify({'error': 'Not found'}), 404)
+
+from flask import request
+
+@app.route('/api/v1.0/quotes', methods=['POST'])
+def create_quote():
+    if not request.json or not 'quote' in request.json:
+        abort(400)
+    quote = {
+        'id': quotes[-1]['id'] + 1,
+        'quote': request.json['quote'],
+        'author': request.json.get('author', "")
+    }
+    quotes.append(quote)
+    return jsonify({'quote': quote}), 201
+
 if __name__ == '__main__':
     app.run(debug=True)
 
